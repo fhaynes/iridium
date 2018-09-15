fe85c38a59f0b8db85c1ba401cfb20fd56a7e804
Fletcher Haynes <fletcher@capitalprawn.com>
2018-09-14 20:27:53 -0700

    Adds crates for SSH server, exposes the VM's ssh module

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,14 @@ extern crate env_logger;
 extern crate log;
 extern crate uuid;
 extern crate num_cpus;
+extern crate thrussh;
+extern crate thrussh_keys;
+extern crate futures;
+extern crate tokio;
 
 pub mod assembler;
 pub mod instruction;
 pub mod repl;
 pub mod scheduler;
 pub mod vm;
+pub mod ssh;
